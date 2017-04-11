e5d6d842055a8f2b5c73531f9022f1be9ed26e6d
Yuliy Vigdorchik <yuliyv@socialnative.com>
2017-04-11 09:12:44 -0700

    Add additional logging to be able to parse out prediction confidences

--- src/image.c
+++ src/image.c
@@ -193,7 +193,7 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
                 alphabet = 0;
             }
 
-            printf("%s: %.0f%%\n", names[class], prob*100);
+            printf(">>>> %s: %.0f%%\n", names[class], prob*100);
             int offset = class*123457 % classes;
             float red = get_color(2,offset,classes);
             float green = get_color(1,offset,classes);
